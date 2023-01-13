ccbff981cddef9b93559655128d5eb3cf7ea87f0
Sivash Viasheslav <sivash.slava@gmail.com>
2023-01-13 14:13:54 +0200

    MarketSportif

---
+++ marketShorts/code/code.js
@@ -0,0 +1,38 @@
+//вывод 4 рандомных товаров на главную страницу
+var goodsContainer = document.querySelector('.block2-container');
+let newGoods = [];
+var out = '';
+for(var key in goods){
+  out = `<div class="block2-container-card">
+    <img src="${goods[key].image}">
+    <div class="block2-container-card-txt1">${goods[key].discription}</div>
+    <div class="rating-mini">
+	<span class="active"></span>	
+	<span class="active"></span>    
+	<span class="active"></span>  
+	<span></span>    
+	<span></span>
+</div>
+    <div class="block2-container-card-txt2"><span>As low as: </span>$${goods[key].cost}</div>
+    <button class="add-to-cart" data-art="${key}"><i class="fa fa-shopping-bag"></i> Add to CARD</button>
+    </div>
+    `
+    newGoods.push(out);
+};
+
+let result = [];
+
+getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
+while (result.length != 4) {
+    let index = getRandomInt(newGoods.length);    
+    result.push(newGoods[index]);    
+    result = result.filter((v, i, arr) =>  arr.indexOf(v) == i);
+};
+result.forEach((item) => {
+  goodsContainer.insertAdjacentHTML('beforeend', item);
+});
+
+//вывод по 10 товаров на страницу товара
+
+//корзина
---
+++ marketShorts/code/products.js
@@ -0,0 +1,402 @@
+const goods = {
+"10001": {
+"name": "Шорти",
+"cost": 99,
+"size": "L",
+"color": "Black",
+"discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+"image": "images/167433686.jpg"
+},
+"10002": {
+    "name": "Шорти",
+    "cost": 299,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10003": {
+    "name": "Шорти",
+    "cost": 199,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10004": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10005": {
+    "name": "Шорти",
+    "cost": 499,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10006": {
+    "name": "Шорти",
+    "cost": 599,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10007": {
+    "name": "Шорти",
+    "cost": 39,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10008": {
+    "name": "Шорти",
+    "cost": 398,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10009": {
+    "name": "Шорти",
+    "cost": 397,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10010": {
+    "name": "Шорти",
+    "cost": 396,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10011": {
+    "name": "Шорти",
+    "cost": 395,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10012": {
+    "name": "Шорти",
+    "cost": 394,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10013": {
+    "name": "Шорти",
+    "cost": 393,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10014": {
+    "name": "Шорти",
+    "cost": 392,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10015": {
+    "name": "Шорти",
+    "cost": 391,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10016": {
+    "name": "Шорти",
+    "cost": 390,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10017": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10018": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10019": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10020": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10021": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10022": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10023": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10024": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10025": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10026": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10027": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10028": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10029": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10030": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10031": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10032": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10033": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10034": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10035": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10036": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10037": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10038": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10039": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10040": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10041": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10042": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10043": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10044": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10045": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10046": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10047": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10048": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10049": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+},
+"10050": {
+    "name": "Шорти",
+    "cost": 399,
+    "size": "L",
+    "color": "Black",
+    "discription": "Шорты Puma Ess Jersey Shorts 58670601 XL Black",
+    "image": "images/167433686.jpg"
+}
+}
